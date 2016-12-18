f050903e8387d0292e3e0c53fbaf6e4f25a43ca2
Georgi Mirchev <gmirchev90@gmail.com>
2016-12-18 13:36:18 +0200

    App Module and App Component

---
+++ app/src/main/java/com/nmp90/pictureminer/di/app/AppComponent.java
@@ -0,0 +1,14 @@
+package com.nmp90.pictureminer.di.app;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by joro on 18.12.16.
+ */
+
+@Singleton
+@Component(modules = {AppModule.class})
+public interface AppComponent {
+}
---
+++ app/src/main/java/com/nmp90/pictureminer/di/app/AppModule.java
@@ -0,0 +1,35 @@
+package com.nmp90.pictureminer.di.app;
+
+import android.content.Context;
+
+import com.nmp90.pictureminer.api.Api;
+import com.nmp90.pictureminer.api.FlickrApi;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by joro on 18.12.16.
+ */
+
+@Module
+public class AppModule {
+    private final Context context;
+
+    public AppModule(Context context) {
+        this.context = context;
+    }
+
+    @Provides
+    Context provideContext() {
+        return context;
+    }
+
+    @Provides
+    @Singleton
+    Api provideApi() {
+        return FlickrApi.createApi();
+    }
+}
